09e0e7b750d5e0deab10d2a0adf4758ce983a948
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-08-12 13:16:53 -0500

    Finished exercises 1-4

---
+++ app.js
@@ -0,0 +1,72 @@
+// Exercise 1: Applying Array.prototype.map()
+
+// Use `.map()` to iterate over the following array:
+
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+// Create a new array where each value is multiplied by 2 and log the new array.
+
+const doubledNums = nums.map(num => num * 2);
+
+console.log(doubledNums);
+
+
+// Exercise 2: Array destructuring
+
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// const [firstTopping, secondTopping] = pizzaToppings;
+
+// console.log(firstTopping);
+// console.log(secondTopping);
+
+
+// Exercise 3: Destructuring objects
+
+// Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
+
+// const car = {
+//     make: 'Audi',
+//     model: 'q5',
+// };
+  
+// const { make, model } = car;
+  
+// console.log(make);
+// console.log(model);
+
+// Exercise 4: Applying the spread operator on arrays
+
+// Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+const controversialPizzaToppings = [...pizzaToppings];
+
+console.log(controversialPizzaToppings);
+
+// Exercise 5: Applying the spread operator on objects
+
+// Duplicate the following object and spread its values into a new variable `myCar`.
+
+const car = {
+  make: 'Audi',
+  model: 'q5',
+};
+
+// Change the `model` property of `myCar` to 'q7'. Log both objects.
+
+// Your code here
+
+
+// Exercise 6: Dynamic keys in objects
+
+// Create an object named userProfile. 
+// Define a variable named propertyName and assign a string to it (like a username, age, or email). 
+// Use propertyName as a dynamic key in userProfile, assigning a relevant value.
+
+// Your code here
+
+
